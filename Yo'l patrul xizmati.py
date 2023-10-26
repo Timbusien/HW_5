af7f6dd5448f2f8fbe27b5115af32352118a01bb
Rustam Abdurashitov <143092592+Timbusien@users.noreply.github.com>
2023-10-26 17:16:45 +0500

    Update Yo'l patrul xizmati.py

--- Yo'l patrul xizmati.py
+++ Yo'l patrul xizmati.py
@@ -30,4 +30,45 @@ hodim_yellow.start()
 hodim_green.start()
 
 
+import time
+def sync_traffic_light():
+    while True:
+        print("Красный")
+        time.sleep(5)
+        print("Желтый")
+        time.sleep(2)
+        print("Зеленый")
+        time.sleep(5)
+
+sync_traffic_light()
+
+
+import asyncio
+
+
+async def red_():
+    while True:
+        await asyncio.sleep(5)
+        print('красный')
+
+
+async def yellow_():
+    while True:
+        await asyncio.sleep(10)
+        print('желтый')
+
+
+async def green_():
+    while True:
+        await asyncio.sleep(10)
+        print('зелёный')
+
+
+potok = asyncio.get_event_loop()
+potok.run_until_complete(red_())
+potok.run_until_complete(yellow_())
+potok.run_until_complete(green_())
+
+
+
 '''oka qizilga otmang!!!'''
